f50dcfd60a6e39acfadbf01b2198f751113b4200
aldenhart <alden08@harts.org>
2010-08-11 09:35:11 -0400

    fixed very long moves - number precision

---
+++ firmware/tinyg_206/controller.h
@@ -0,0 +1,29 @@
+/*
+ * tg_controller.h - tinyg controller and top level parsers
+ * Part of TinyG project
+ * Copyright (c) 2010 Alden S. Hart, Jr.
+ *
+ * TinyG is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * TinyG is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with TinyG  
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef controller_h
+#define controller_h
+
+/*
+ * Global Scope Functions
+ */
+
+void tg_init(void);
+void tg_controller(void);
+int tg_parser(char * buf);
+void tg_reset_source();
+
+#endif
--- firmware/tinyg_206/default/dep/motion_control.o.d
+++ firmware/tinyg_206/default/dep/motion_control.o.d
@@ -1,11 +1,11 @@
-controller.o: ../controller.c \
-  c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h \
+motion_control.o: ../motion_control.c ../xmega_init.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
-  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h \
   c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/ctype.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/math.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/iox256a3.h \
@@ -13,26 +13,26 @@ controller.o: ../controller.c \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
   c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h ../xio.h \
-  ../xio_file.h ../xio_usart.h ../tinyg.h ../controller.h ../gcode.h \
-  ../config.h ../move_buffer.h ../motion_control.h \
-  c:/winavr-20100110/lib/gcc/../../avr/include/string.h ../direct_drive.h \
-  ../stepper.h ../data_gcode_asst.h ../data_gcode_zoetrope.h \
-  ../data_gcode_contraptor_circle.h
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h ../tinyg.h \
+  ../config.h ../motion_control.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/string.h ../move_buffer.h \
+  ../stepper.h
 
-c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h:
+../xmega_init.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
 
 c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
 
 c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
 
-c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h:
 
 c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
 
-c:/winavr-20100110/lib/gcc/../../avr/include/ctype.h:
-
-c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+c:/winavr-20100110/lib/gcc/../../avr/include/math.h:
 
 c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
 
@@ -50,32 +50,14 @@ c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
 
 c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
 
-../xio.h:
-
-../xio_file.h:
-
-../xio_usart.h:
-
 ../tinyg.h:
 
-../controller.h:
-
-../gcode.h:
-
 ../config.h:
-
-../move_buffer.h:
 
 ../motion_control.h:
 
 c:/winavr-20100110/lib/gcc/../../avr/include/string.h:
 
-../direct_drive.h:
+../move_buffer.h:
 
 ../stepper.h:
-
-../data_gcode_asst.h:
-
-../data_gcode_zoetrope.h:
-
-../data_gcode_contraptor_circle.h:
