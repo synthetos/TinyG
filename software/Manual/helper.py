ec69bb30dea06b88a3c4c567aca8fb5f13763bd9
unknown <ril3y@.(none)>
2010-08-24 23:10:59 -0400

    Removed old dir

--- software/Manual/helper.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#helper function
-
-def PrintDebug(msg, Widget):
-    """Accepts an TextCtrl Box Widget and the MSG you want to display"""
-    return self.Widget.AppendText("\n"+msg)
-    
