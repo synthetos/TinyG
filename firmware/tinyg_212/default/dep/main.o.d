20da261307ee850c1e1d436966d33ffac309df97
aldenhart <alden08@harts.org>
2010-10-02 20:42:52 -0400

    Try out 212 (bezor)

---
+++ firmware/tinyg_212/default/dep/main.o.d
@@ -0,0 +1,97 @@
+main.o: ../main.c c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iox256a3.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  ../hardware.h ../xmega_init.h ../xmega_interrupts.h ../xmega_eeprom.h \
+  ../xio.h c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  ../xio_file.h ../xio_usart.h ../tinyg.h ../controller.h ../network.h \
+  ../config.h ../stepper.h ../limit_switches.h ../move_buffer.h \
+  ../motion_control.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/string.h ../spindle.h \
+  ../direct_drive.h ../encoder.h ../gcode.h
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iox256a3.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
+
+../hardware.h:
+
+../xmega_init.h:
+
+../xmega_interrupts.h:
+
+../xmega_eeprom.h:
+
+../xio.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+../xio_file.h:
+
+../xio_usart.h:
+
+../tinyg.h:
+
+../controller.h:
+
+../network.h:
+
+../config.h:
+
+../stepper.h:
+
+../limit_switches.h:
+
+../move_buffer.h:
+
+../motion_control.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/string.h:
+
+../spindle.h:
+
+../direct_drive.h:
+
+../encoder.h:
+
+../gcode.h:
