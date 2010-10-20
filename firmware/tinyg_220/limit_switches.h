d44b88780735e2263ff0c92b8ebc8416fbdb433c
aldenhart <alden08@harts.org>
2010-10-20 16:18:02 -0400

    added homing cycle

---
+++ firmware/tinyg_220/limit_switches.h
@@ -0,0 +1,42 @@
+/*
+ * limit_switches.t - limit switch interfaces
+ * Part of TinyG project
+ * Copyright (c) 2010 Alden S Hart, Jr.
+ *
+ * TinyG is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * TinyG is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with TinyG  
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef limit_switches_h
+#define limit_switches_h
+
+/*
+ * Global Scope Functions and Data
+ */
+
+struct lsStruct {
+//	volatile uint8_t state;
+	volatile uint8_t count;
+	volatile uint8_t x_min;	// 0=open, 1=hit
+	volatile uint8_t x_max;
+	volatile uint8_t y_min;
+	volatile uint8_t y_max;
+	volatile uint8_t z_min;
+	volatile uint8_t z_max;
+	volatile uint8_t a_min;
+	volatile uint8_t a_max;
+};
+struct lsStruct ls;
+
+void ls_init(void);
+void ls_clear_limit_switches(void);
+
+#endif
---
+++ firmware/tinyg_221/xmega_rtc.c
@@ -0,0 +1,70 @@
+/*
+ * xmega_rtc.h - real-time counter/clock
+ * Part of TinyG project
+ *
+ * Copyright (c) 2010 Alden S. Hart, Jr.
+ *
+ * TinyG is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your 
+ * (option) any later version.
+ *
+ * TinyG is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with TinyG  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "tinyg.h"
+#include "limit_switches.h"
+#include "xmega_rtc.h"
+
+#define RTC_TICKS 10		// interrupt on every 10 ticks (~10 ms)
+
+/* 
+ * rtc_init() - initialize and start the clock
+ *
+ * The order of the instructions in this routine follows app note 1314.
+ * Best not to mess with it.
+ */
+
+void rtc_init()
+{
+	do {
+		// wait for syncbusy to clear
+	} while (RTC.STATUS);
+
+	// RTC register setup
+	CLK.RTCCTRL	= 0x05;					// use internal RTC 32.768, ENABLE
+	RTC.PER = RTC_TICKS;				// overflow period
+	RTC.COMP = RTC_TICKS;				// overflow period
+	RTC.CNT = 0;
+	RTC.INTCTRL = RTC_COMPINTLVL_LO_gc;	// lo interrupt on overflow
+	RTC.CTRL = RTC_PRESCALER_DIV1_gc;	// no prescale (1x)
+
+	// app derived clocks
+	rtc.clock_ms = 0;
+}
+
+
+ISR(RTC_COMP_vect)
+{
+	if (ls.count) {		// limit switch lockout counter (for debouncing)
+		ls.count--;
+	}
+	++rtc.clock_ms;
+}
+
+void rtc_reset_ms()
+{
+//	RTC.INTCTRL = RTC_OVFINTLVL_OFF_gc;	// disable interrupt
+	rtc.clock_ms = 0;
+//	RTC.INTCTRL = RTC_OVFINTLVL_LO_gc;	// enable interrupt
+}
+
