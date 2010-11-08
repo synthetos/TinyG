6355406ef7bbda2c3618021ca5fa159c63761045
aldenhart <alden08@harts.org>
2010-11-07 20:13:26 -0500

    work on homing cycle

--- firmware/tinyg_221/controller.c
+++ firmware/tinyg_221/controller.c
@@ -1,6 +1,7 @@
 /*
  * controller.c - tinyg controller and top level parser
  * Part of TinyG project
+ *
  * Copyright (c) 2010 Alden S. Hart, Jr.
  *
  * TinyG is free software: you can redistribute it and/or modify it under
@@ -16,9 +17,64 @@
  * You should have received a copy of the GNU General Public License 
  * along with TinyG  If not, see <http://www.gnu.org/licenses/>.
  *
+ * ---- Controller Operation ----
+ *
+ *	The controller implements a simple process control scheme to manage 
+ *	blocking of multiple "threads" in the application. Technically, the 
+ *	controller implements a simple event-drive state machine using inverted
+ *	control to manage a set of cooperative run-to-completion kernel tasks.
+ * 	(ref: http://www.state-machine.com/products)
+ *
+ *	More simply, it works as a aborting "superloop", where the highest 
+ *	priority tasks are run first and progressively lower priority tasks 
+ *	are run only if the higher priority tasks are not blocked. No task 
+ *	should ever actually block, but instead return "busy" when it would
+ *	ordinarily block, and provide a re-entry point to resume the task
+ *	once the blocking condition has been removed.
+ *
+ *	For this to work tasks must be written to run-to-completion (non 
+ *	blocking), and must offer re-entry points (continuations) to resume
+ *	operations that would have blocked (see arc generator for an example). 
+ *	A task returns TG_EAGAIN to indicate a blocking point. If TG_EAGAIN 
+ *	is received The controller quits the loop and starts over. Any other 
+ *	return code allows the controller to proceed down the task list.
+ *	(See end notes in this file for how to write a continuation).
+ *
+ *	Interrupts run at the highest priority level; kernel tasks are 
+ *	organized into priority groups below the interrupt levels. The 
+ *	priority of operations is:
+ *
+ *	- High priority ISRs
+ *		- issue steps to motors / count dwell timings
+ *		- dequeue and load next stepper move
+ *
+ *	- Medium priority ISRs
+ *		- receive serial input (RX)
+ *		- execute signals received by serial input
+ *		- detect and flag limit switch closures
+ *
+ *	- Low priority ISRs
+ *		- send serial output (TX)
+ *
+ *	- Main loop tasks
+ *		These are divided up into layers depending on priority and blocking
+ *		hierarchy. See tg_controller() for details.
+ *
+ *	Notes:
+ *	  - Gcode and other command line flow control is managed cooperatively
+ *		with the application sending Gcode or other commands. The '*' 
+ *		char in the prompt indicates that the controller is ready for the 
+ *		next line. The sending app is supposed to honor this and not stuff
+ *		lines down the pipe (which will choke the controller).
+ *
+ *	Futures: Using a super loop instead of an event system is a design 
+ *	tradoff - or more to the point - a hack. If the flow of control gets 
+ *	much more complicated it will make sense to replace this section 
+ *	with an event driven dispatcher.
+ *
  * ---- Mode Auto-Detection behaviors ----
  *
- *	The first letter of an IDLE mode  line performs the following actions
+ *	The first letter of an IDLE mode line performs the following actions
  *
  *		G,M,N,F,%,(	enter GCODE_MODE (as will lower-case of the same)
  *		C,?			enter CONFIG_MODE
@@ -31,64 +87,6 @@
  *
  *	Once in the selected mode these characters are not active as mode 
  *	selects. Most modes use Q (Quit) to exit and return to idle mode.
- *
- * ---- Controller Operation ----
- *
- *	The controller implements a simple process control scheme to manage 
- *	blocking in the application. The controller works as a aborting "super
- *	loop", where the highest priority tasks are run first and progressively
- *	lower priority tasks are run only if the higher priority tasks are not 
- *	blocked.
- *
- *	For this to work tasks must be written to run-to-completion (non 
- *	blocking), and must offer re-entry points (continuations) to resume
- *	operations that would have blocked (see arc generator for an example). 
- *	A task returns TG_EAGAIN to indicate a blocking point. If TG_EAGAIN 
- *	is received The controller quits the loop and starts over.Any other 
- *	return code allows the controller to proceed down the list.
- *
- *	Interrupts run at the highest priority level.
- *
- *	The priority of operations is:
- *
- *	- High priority ISRs
- *		- issue steps to motors
- *		- count dwell timings
- *		- dequeue and load next stepper move
- *
- *	- Medium priority ISRs
- *		- receive serial input (RX)
- *		- execute signals received by serial input
- *
- *	- Low priority ISRs
- *		- send serial output (TX)
- *
- *	- Top priority tasks
- *		- dequeue and load next stepper move (only if stalled by ISRs)
- *
- *  - Medium priority tasks
- *		- line generator continuation - queue line once move buffer is ready
- *		- arc generator continuation - queue arc segments once move buffer...
- *
- *  - Low priority tasks
- *		- read line from active input device. On completed line:
- *			- run gcode interpreter (or other parser)
- *			- run motion control called by the gcode interpreter
- *			- queue lines and arcs (line and arc generators)
- *		- send "receive ready" * back to input source (*'s via _tg_prompt())
- *			(does this once and only once a parser has returned)
- *
- *	Notes:
- *	  - Gcode and other command line flow control is managed cooperatively
- *		with the application sending the Gcode or other command. The '*' 
- *		char in the prompt indicates that the controller is ready for the 
- *		next line. The sending app is supposed to honor this and not stuff
- *		lines down the pipe (which will choke the controller).
- *
- *	Futures: Using a super loop instead of an event system is a design 
- *	tradoff - or more to the point - a hack. If the flow of control gets 
- *	much more complicated it will make sense to replace this section 
- *	with an event driven dispatcher.
  */
 
 #include <stdio.h>
@@ -105,6 +103,7 @@
 #include "motion_control.h"
 #include "direct_drive.h"
 #include "stepper.h"			// needed for stepper kill and terminate
+#include "limit_switches.h"
 #include "xmega_eeprom.h"
 
 /*
@@ -121,25 +120,10 @@
  * Local Scope Functions and Data
  */
 
-struct tgController tg;
+struct tgController tg;			// controller state structure
 
-enum tgControllerState {		// command execution state
-	TG_READY_UNPROMPTED,		// ready for input, no prompt sent
-	TG_READY_PROMPTED,			// ready for input, prompt has been sent
-	TG_STATE_MAX
-};
-
-#define TG_FLAG_PROMPTS_bm (1<<0)// prompt enabled if set
-
-enum tgMode {
-	TG_IDLE_MODE,				// idle mode only. No other modes active
-	TG_CONFIG_MODE,				// read and set configurations
-	TG_GCODE_MODE,				// gcode interpreter
-	TG_DIRECT_DRIVE_MODE,		// direct drive motors
-	TG_MAX_MODE
-};
-
-// local helper functions
+static int _tg_parser(char * buf);
+static int _tg_read_next_line(void);
 static void _tg_prompt(void);
 static void _tg_set_mode(uint8_t mode);
 static void _tg_set_source(uint8_t d);
@@ -148,7 +132,11 @@ static int _tg_test(void);
 //static int _tg_mudflap_file(void);
 
 /*
- * tg_init()
+ * tg_init() - controller init
+ * tg_alive() - announce that TinyG is alive
+ *
+ *	The controller init is split in two: the actual init, and tg_alive()
+ *	which should be issued once the rest of he application is initialized.
  */
 
 void tg_init() 
@@ -160,61 +148,112 @@ void tg_init()
 	tg.state = TG_READY_UNPROMPTED;
 }
 
-/*
- * tg_alive() - announce that TinyG is alive
- */
-
 void tg_alive() 
-{
+{										 // see tinyg.h for string
 	printf_P(PSTR("**** TinyG %S ****\n"), (PSTR(TINYG_VERSION)));
 	_tg_prompt();
 }
 
 /* 
- * tg_controller() - top-level controller.
+ * tg_controller() - top-level controller
  *
  * Tasks are ordered by increasing dependency (blocking hierarchy) - tasks 
  * that are dependent on the completion of lower-level tasks should be
  * placed later in the list than the task(s) they are dependent upon.
+ * Any task in the list must be written as a continuation. See end notes
+ * in this file for how to code continuations.
  */
 
 #define	DISPATCH(func) switch (func) { \
 	case (TG_EAGAIN): { return; } \
-	case (TG_OK): { if (cfg.homing_cycle_active) { return; } \
+	case (TG_OK): { if (cfg.cycle_active) { return; } \
 					else { tg.state = TG_READY_UNPROMPTED; _tg_prompt(); return; \
 				  } } }
-	// any other condition drops through and runs the next routine in the list
+	// Any other condition drops through and runs the next routine in the list
+	// A routine returning TG_EAGAIN pre-empts the rest of the list
+	// A routine returning TG_OK will cause a prompt to occur
+	// A routine that had no action (i.e. is OFF or idle) should return TG_NOOP 
 
 void tg_controller()
 {
-	st_execute_move();					// always start with this
+	// level 0 routines - main loop routines invoked from interrupts
+	DISPATCH(ls_handler());				// limit switch handler
+//	DISPATCH(tg_process_end());			// complete processing ENDs
 
-	// level 0 routines - move queue primitives
+	// level 1 routines - stepper and motion primitives
+	st_execute_move();					// always do this (no dispatch)
 	DISPATCH(mc_line_continue());
 	DISPATCH(mc_dwell_continue());
 	DISPATCH(mc_queued_start_stop_continue());
-
-	// level 1 routines - motion primitives
 	DISPATCH(mc_arc_continue());
 
 	// level 2 routines - canonical machine cycles
 	DISPATCH(cm_return_to_home_continue());
 
 	// level 3 routines - parsers and line readers
-	DISPATCH(tg_read_next_line());
-
-	_tg_prompt();						// always end with this
+	DISPATCH(_tg_read_next_line());
+	
+	// level N - always end with the prompt
+	_tg_prompt();
 }
 
 /* 
- * tg_read_next_line() - Perform a non-blocking line read from active input device
+ * tg_application_startup() - application start and restart
  */
 
-int tg_read_next_line()
+uint8_t tg_application_startup(void)
+{
+	tg.status = TG_OK;					// pre-emptively set
+
+	// inititate homing cycle
+	if (cfg.homing_mode == TRUE) { 
+		tg.status = cm_return_to_home();
+	}
+
+	// Pre-load the USB RX (input) buffer with some test strings
+	// Be mindful of the char limit on the RX_BUFFER_SIZE (circular buffer)
+
+//	xio_queue_RX_string_usb("?\n");		// enter config mode and dump config
+//	xio_queue_RX_string_usb("Q\n");		// go to idle mode
+//	xio_queue_RX_string_usb("R\n");		// run a homing cycle
+//	xio_queue_RX_string_usb("Q\n");		// go to idle mode
+//	xio_queue_RX_string_usb("T\n");		// run test file
+
+//	xio_queue_RX_char_usb(ETX);			// send control-c (kill)
+//	xio_queue_RX_char_usb(XOFF);		// send control-s (pause)
+
+//	xio_queue_RX_string_usb("(MSGtest message in comment)\n");
+//	xio_queue_RX_string_usb("g1 f450 x10 y13\n");
+//	xio_queue_RX_string_usb("g0x0y0z0\n");
+//	xio_queue_RX_string_usb("g0x1000\n");
+//	xio_queue_RX_string_usb("g0x10000\n");
+//	xio_queue_RX_string_usb("g0x10\ng4p1\ng0x0\n");
+//	xio_queue_RX_string_usb("g0 x-10 (MSGtest)\n");
+//	xio_queue_RX_string_usb("g0 x10 y11 z12\n");
+//	xio_queue_RX_string_usb("g0 x0 y0 z0\n");
+//	xio_queue_RX_string_usb("g2 x10 y10 z0 i5 j5 f200\n");
+//	xio_queue_RX_string_usb("g2 f300 x10 y10 i8 j8\n");
+//	xio_queue_RX_string_usb("g92 x0 y0 z0\n");
+//	xio_queue_RX_string_usb("g0x10y10z0\n");
+//	xio_queue_RX_string_usb("g91g0x5y5\n");
+//	xio_queue_RX_string_usb("g1 f200 x10\n");
+//	xio_queue_RX_string_usb("y10\n");
+//	xio_queue_RX_string_usb("g0 x100 y100 z100 a100\n");
+//	xio_queue_RX_string_usb("?\n");
+
+	return (tg.status);
+}
+
+
+/* 
+ * _tg_read_next_line() - Perform a non-blocking line read from active input device
+ */
+
+int _tg_read_next_line()
 {
 	// read input line or return if not a completed line
 	if ((tg.status = xio_gets(tg.src, tg.buf, sizeof(tg.buf))) == TG_OK) {
-		tg.status = tg_parser(tg.buf);			// dispatch to parser
+		tg.status = _tg_parser(tg.buf);			// dispatch to parser
 	}
 
 	// Note: This switch statement could be reduced as most paths lead to
@@ -235,7 +274,7 @@ int tg_read_next_line()
 		case TG_EOF: {							// EOF from file devs only
 			printf_P(PSTR("End of command file\n"));
 			tg_reset_source();					// reset to default src
-			xio_queue_RX_string_usb("Q\nT\n");	// +++++ run test file again (forever)
+//			xio_queue_RX_string_usb("Q\nT\n");	// +++++ run test file again (forever)
 			tg.state = TG_READY_UNPROMPTED;
 			break;
 		}
@@ -247,7 +286,7 @@ int tg_read_next_line()
 }
 
 /* 
- * tg_parser() - process top-level serial input 
+ * _tg_parser() - process top-level serial input 
  *
  *	tg_parser is the top-level of the input parser tree; dispatches other 
  *	parsers. Calls lower level parser based on mode
@@ -263,7 +302,7 @@ int tg_read_next_line()
  *	Preserves and passes through return codes (status codes) from lower levels
  */
 
-int tg_parser(char * buf)
+int _tg_parser(char * buf)
 {
 	// auto-detect mode if not already set 
 	if (tg.mode == TG_IDLE_MODE) {
@@ -301,12 +340,18 @@ void _tg_set_mode(uint8_t mode)
 }
 
 /*
+ * tg_reset_source()  Reset source to default input device
  * _tg_set_source()  Set current input source
  *
  * Note: Once multiple serial devices are supported this function should 
  *	be expanded to also set the stdout/stderr console device so the prompt
  *	and other messages are sent to the active device.
  */
+
+void tg_reset_source()
+{
+	_tg_set_source(tg.default_src);
+}
 
 void _tg_set_source(uint8_t d)
 {
@@ -316,15 +361,6 @@ void _tg_set_source(uint8_t d)
 	} else {
 		tg.flags |= TG_FLAG_PROMPTS_bm;
 	}
-}
-
-/*
- * tg_reset_source()  Reset source to default input device
- */
-
-void tg_reset_source()
-{
-	_tg_set_source(tg.default_src);
 }
 
 /* 
@@ -428,8 +464,14 @@ void tg_print_status(const uint8_t status_code, const char *textbuf)
 
 int _tg_reset(void)
 {
-	tg_app_reset();						// application reset from main
-	tg.status = cm_return_to_home();	// do a homing cycle
+//	tg_application_init();				// application hard init
+	tg.status = tg_application_startup();			// application startup sequence
+
+	if (cfg.homing_mode) {
+		tg.status = cm_return_to_home(); // do a homing cycle
+	} else {
+		tg.status = (TG_OK);
+	}
 	return(tg.status);
 }
 
@@ -446,7 +488,6 @@ int _tg_test(void)
 	xio_open_pgm(PGMFILE(&hokanson_02)); 	// contraptor circle test
 	_tg_set_source(XIO_DEV_PGM);
 	_tg_set_mode(TG_GCODE_MODE);
-//	xio_queue_RX_string_usb("Q\nT\n");	// run test file again (forever)
 	return (TG_OK);
 }
 
@@ -517,7 +558,7 @@ int _tg_mudflap_file()
 		- semicolon
 		- NUL
 
-  Design notes:
+---- Design notes ----
   	- line readers are the lowest level (above single character read)
 		From serial inputs: read single characters to assemble a string
 		From in-memory strings: read characters from a string in program memory
@@ -540,5 +581,44 @@ int _tg_mudflap_file()
 		Flow control is provided by the called routine running to completion 
 		without blocking. If blocking could occur (e.g. move buffer is full)
 		the routine should return and provide a continuation in the main 
-		controller loop. THis necessitates some careful state handling.
+		controller loop. This necessitates some careful state handling.
+
+---- Coding Continuations ----
+
+	Continuations are used to manage points where the application would 
+	ordinarily block. Call it application managed threading by way of an 
+	inverted control loop. By coding using continuations the application 
+	does not need an RTOS and is extremely responsive (there are no "ticks")
+
+	Rules for writing a continuation task:
+	  - A continuation is a pair of routines. The first is the main routine,
+		the second the continuation. See mc_line() and mc_line_continue().
+
+	  - The main routine is called first. It may have args. It performs 
+	    all initial actions and sets up a static structure to hold data
+		that is needed by successive calls to the continuation. The main
+		routine usually ends by calling the continuation inside a return().
+
+	  - The task is called initially by calling the main routine, then will 
+	  	be called repeatedly from the main controller loop. The continuation 
+		cannot have input args - all necessary data should be available in 
+		the static struct.
+
+	  - What happens in the continuation is managed by the continuation state
+	  	which is typically  one of: OFF, NEW, or RUNNING. The state on initial 
+		entry from the main routine should be NEW.
+
+	  - The continuation must return the following codes, and may return 
+	  	additional codes to indicate various exception conditions:
+
+		  -	TG_NOOP: No operation ocurred. This is the usual return from an 
+		  			 OFF state. All continuations must be callable with no 
+					 effect when they are OFF (as they are called repeatedly 
+					 whether or not they are active).
+
+		  - TG_AGAIN: Continuation is blocked or still processing (RUNNING).
+
+		  - TG_OK; 	 Continuation task  has just is completed - i.e. it has 
+		  			 just transitioned to OFF. TG_OK should only be returned 
+					 once. The next state will be OFF, which will return NOOP.
 */
