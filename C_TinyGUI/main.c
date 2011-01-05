f399743f02028608cf0f40c8f42288bc50ea228f
ril3y <rileyporter@gmail.com>
2011-01-04 19:34:44 -0500

    Inital Attempt to Port Gcode layer to a standalone application

---
+++ C_TinyGUI/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "motion_control.h"
+#include "canonical_machine.h"
+#include "config.h"
+#include "gcode.h"
+
+
+int main (int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
