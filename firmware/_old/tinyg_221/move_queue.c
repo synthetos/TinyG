a23fbdd6d76eb58684fc2dfb3b4352635cc0ff48
aldenhart <alden08@harts.org>
2011-02-16 16:01:31 -0500

    more work on short-line optimization

---
+++ firmware/_old/tinyg_221/move_queue.c
@@ -0,0 +1,286 @@
+/*
+ * move_buffer.c - routines for managing motor moves
+ * Part of TinyG project
+ *
+ * Copyright (c) 2010 Alden S. Hart, Jr.
+ * Portions if this module copyright (c) 2009 Simen Svale Skogsrud
+ *
+ * TinyG is free software: you can redistribute it and/or modify it under the 
+ * terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * TinyG is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with TinyG  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ------
+ * This code buffers pre-computed moves to optimize dequeuing / loading time
+ *
+ *	Instead of queueing the move as:
+ *		- steps_x
+ *		- steps_y
+ *		- steps_z
+ *		- microseconds (length of move),
+ *
+ *	move is pre-computed and carried as the values needed by the stepper ISRs:
+ *	  ...for each axis:
+ *		- steps
+ *		- timer period
+ *		- timer postscaler value
+ *		- direction 
+ *
+ *	This moves an expensive 64 bit division operation (~3800 cycles X 3) 
+ *	to this phase and keeps it out of the high-priority stepper ISRs. 
+ *	This makes for smooth movement (changes between lines) at high speeds, 
+ *	and and helps to run the RS-485 network at high speeds (med priority ISRs).  
+ *	See build 184 for the non-optimized version.
+ */
+
+#include <stdlib.h>
+#include <avr/io.h>
+#include <string.h>								// for memset();
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include "move_queue.h"
+#include "stepper.h"							// for 	st_execute_move()
+#include "config.h"
+#include "tinyg.h"
+
+/* 
+ * Local Scope Data and Functions
+ */
+
+//#define MOVE_BUFFER_SIZE 150
+#define MOVE_BUFFER_SIZE 3						// limited to 255 unless you change:
+												// move_buffer_head (to uint16_t)
+												// move_buffer_tail (to uint16_t)
+												// next_buffer_head (multiple places)
+struct mvMoveBuffer {
+	uint64_t ticks;								// see note.
+	uint64_t ticks_per_step;
+	uint64_t microseconds;
+	volatile uint8_t move_buffer_head;			// move queue index (for writes)
+	volatile uint8_t move_buffer_tail;			// move dequeue index (for reads)
+	struct mvMove *p;							// move buffer pointer
+	struct mvMove move_buffer[MOVE_BUFFER_SIZE];// move buffer storage
+};
+static struct mvMoveBuffer mv;
+
+/*	Note: 64 bit fixed point arithmetic is used to compute ticks, steps and 
+	durations (seconds) while queuing moves. A 2 minute move overflows 32 bits. 
+	Using 64 bits is expensive. The division goes from ~640 cycles at 32 bits
+	to ~3800 cycles using 64 bits. Can't use doubles as you need to manipulate 
+	the bits to load the timers.
+*/
+
+/* 
+ * mv_init() - initialize move buffers
+ */
+
+void mv_init()
+{
+	mv.move_buffer_head = 0;
+	mv.move_buffer_tail = 0;
+}
+
+/*
+ * mv_queue_line() - Add a new linear movement to the move buffer
+ *
+ * Arguments:
+ *	steps_x, steps_y and steps_z are signed relative motion in steps 
+ *	Microseconds specify how many microseconds the move should take to perform
+ *
+ * Blocking behavior:
+ *	This routine returns BUFFER _FULL if there is no space in the buffer.
+ *	A version that sleeps (blocks) if there is no space in the move buffer is
+ *  commented out. If you want to run non-blocking, first call 
+ *  mv_test_move_buffer_full() to test the queue. Or package the two functions 
+ *	in a non-blocking wrapper.
+ *
+ * Move buffer circular buffer operation
+ *  move_buffer_tail is the array index from which the previous line was read.
+ *	move_buffer_tail is always incremented before reading the line.
+ *
+ *	move_buffer_head is the array index to which the line will be written.
+ *	move_buffer_head is always incremented after writing the line.
+ *
+ *	Buffer full:	move_buffer_head == move_buffer_tail
+ *	Buffer empty:	move_buffer_head == move_buffer_tail+1 
+ */
+
+uint8_t mv_queue_line(int32_t steps_x, int32_t steps_y, 
+					  int32_t steps_z, int32_t steps_a, 
+					  uint32_t microseconds)
+{
+	uint8_t next_buffer_head;
+	uint8_t i;
+
+	// Determine the buffer head index needed to store this line
+	if ((next_buffer_head = mv.move_buffer_head + 1) >= MOVE_BUFFER_SIZE) {
+		next_buffer_head = 0;					 // wrap condition
+	}
+
+	// Return with error if the buffer is full 
+	if (mv.move_buffer_tail == next_buffer_head) {
+		return (TG_BUFFER_FULL_NON_FATAL);
+//		sleep_mode();	// USE INSTEAD IF YOU WANT BLOCKING BEHAVIOR
+	}
+
+	// setup the move struct and ticks value
+	mv.p = &mv.move_buffer[mv.move_buffer_head];
+	memset(mv.p, 0, sizeof(struct mvMove));
+	mv.p->a[X].steps = steps_x;
+	mv.p->a[Y].steps = steps_y;
+	mv.p->a[Z].steps = steps_z;
+	mv.p->a[A].steps = steps_a;
+	mv.microseconds = (uint64_t)microseconds;	// cast to larger base
+	mv.ticks = mv.microseconds * TICKS_PER_MICROSECOND;
+
+	for (i = 0; i <= AXES; i++) {
+		if (mv.p->a[i].steps) { 				// skip axes w/ zero steps
+			// set direction: (polarity is corrected during execute move)
+			(mv.p->a[i].steps < 0) ? 
+			(mv.p->a[i].direction = 1): 		// CCW = 1 
+			(mv.p->a[i].direction = 0);			// CW = 0
+
+			// set steps to absolute value
+			mv.p->a[i].steps = labs(mv.p->a[i].steps);
+
+			// Normalize ticks_per_step by right shifting until the MSword = 0
+			// Accumulate LSBs shifted out of ticks_per_step into postscale
+			mv.p->a[i].postscale = 1;
+			mv.ticks_per_step = (uint64_t)(mv.ticks / mv.p->a[i].steps);// expensive!
+			while (mv.ticks_per_step & 0xFFFFFFFFFFFF0000) {
+				mv.ticks_per_step >>= 1;
+				mv.p->a[i].postscale <<= 1;
+			}
+			mv.p->a[i].period = (uint16_t)(mv.ticks_per_step & 0x0000FFFF);
+		}
+	}
+	mv.p->move_type = MOVE_TYPE_LINE;
+	mv.move_buffer_head = next_buffer_head;
+	st_execute_move();			// kick the stepper drivers
+	return (TG_OK);
+}
+
+/*
+ * mv_queue_dwell() - Add a dwell to the move buffer
+ *
+ * Queue a dwell on the Z axis
+ */
+
+uint8_t mv_queue_dwell(uint32_t microseconds)
+{
+	uint8_t next_buffer_head;
+
+	// Determine the buffer head index needed to store this line
+	if ((next_buffer_head = mv.move_buffer_head + 1) >= MOVE_BUFFER_SIZE) {
+		next_buffer_head = 0;					 // wrap condition
+	}
+
+	// Return with error if the buffer is full 
+	if (mv.move_buffer_tail == next_buffer_head) {
+		return (TG_BUFFER_FULL_NON_FATAL);
+//		sleep_mode();	// USE INSTEAD IF YOU WANT BLOCKING BEHAVIOR
+	}
+
+	// setup the move struct and ticks value
+	mv.p = &mv.move_buffer[mv.move_buffer_head];
+	memset(mv.p, 0, sizeof(struct mvMove));
+	mv.microseconds = (uint64_t)microseconds;			// cast to larger base
+	mv.ticks = mv.microseconds * TICKS_PER_MICROSECOND;
+	mv.p->a[Z].steps = (((mv.ticks & 0xFFFF0000)>>32)+1);	// compute steps
+	mv.p->a[Z].postscale = 1;
+	mv.ticks_per_step = (uint64_t)(mv.ticks / mv.p->a[Z].steps); // expensive!
+	while (mv.ticks_per_step & 0xFFFFFFFFFFFF0000) {
+		mv.ticks_per_step >>= 1;
+		mv.p->a[Z].postscale <<= 1;
+	}
+	mv.p->a[Z].period = (uint16_t)(mv.ticks_per_step & 0x0000FFFF);
+	mv.p->move_type = MOVE_TYPE_DWELL;
+	mv.move_buffer_head = next_buffer_head;
+	st_execute_move();
+	return (TG_OK);
+}
+
+/*
+ * mv_queue_start_stop() - Add a start or stop to the move buffer
+ */
+
+uint8_t mv_queue_start_stop(uint8_t move_type)
+{
+	uint8_t next_buffer_head;
+
+	// Determine the buffer head index needed to store this line
+	if ((next_buffer_head = mv.move_buffer_head + 1) >= MOVE_BUFFER_SIZE) {
+		next_buffer_head = 0;					 // wrap condition
+	}
+	if (mv.move_buffer_tail == next_buffer_head) {
+		return (TG_BUFFER_FULL_NON_FATAL);
+//		sleep_mode();	// USE INSTEAD IF YOU WANT BLOCKING BEHAVIOR
+	}
+	mv.p->move_type = move_type;
+	mv.move_buffer_head = next_buffer_head;
+	st_execute_move();
+	return (TG_OK);
+}
+
+/* 
+ * mv_dequeue_move_buffer() - Return next move from buffer & advance buffer tail
+ */
+
+struct mvMove *mv_dequeue_move_buffer()
+{
+	if (mv.move_buffer_head == mv.move_buffer_tail) {	// buffer empty
+		return (NULL);
+	}
+	mv.p = &mv.move_buffer[mv.move_buffer_tail];	// get and save current index
+	if (++mv.move_buffer_tail >= MOVE_BUFFER_SIZE) { // increment and wrap (no OBOE)
+		mv.move_buffer_tail = 0;
+	}
+	return (mv.p); 
+}
+
+/*
+ * mv_test_move_buffer_full() - Test if the move buffer is full
+ *
+ *	Returns:	1 (TRUE) 	the buffer is full
+ *  			0 (FALSE) 	the buffer is not full
+ *
+ *  Note: The method of testing for buffer full then writing the buffer as a
+ *		  separate, non-atomic operation works as long as there is no pre-emption
+ *		  that could invalidate the full/not-full result. As this project is
+ *		  currently coded there is no pre-emption possible in this critical region -
+ *		  i.e. there's no way somebody else can get in there and write to the line 
+ *		  buffer between the not-full result and the subsequent write. 
+ *		  Be careful about changing this condition.
+ */
+
+uint8_t mv_test_move_buffer_full() 
+{
+	uint8_t next_buffer_head;
+
+	if ((next_buffer_head = mv.move_buffer_head + 1) >= MOVE_BUFFER_SIZE) {
+		next_buffer_head = 0;					  // wrap condition
+	}
+	if (mv.move_buffer_tail == next_buffer_head) { 	// test if full
+		return (TRUE);
+	};
+	return (FALSE);
+}
+
+/* 
+ * mv_flush() - remove all buffered moves (reset queue) 
+ */
+
+void mv_flush()
+{
+	cli();
+	mv.move_buffer_tail = mv.move_buffer_head;
+	mv.p = NULL;
+	sei();
+}
---
+++ firmware/_old/tinyg_224/settings.h
@@ -0,0 +1,197 @@
+/*
+ * settings.h - system configuration values
+ * Part of TinyG project
+ *
+ * Copyright (c) 2010 Alden S. Hart, Jr.
+ *
+ * TinyG is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * TinyG is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with TinyG  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* Note: The values in this file are the default settings that are loaded
+ * 		 into a virgin EEPROM, and can be changed using the config commands.
+ *		 After initial load the EEPROM values (or changed values) are used.
+ *
+ *		 System and hardware settings that you shouldn't need to change 
+ *		 are in system.h  Application settings that also shouldn't need 
+ *		 to be changed are in tinyg.h
+ */
+
+#ifndef settings_h
+#define settings_h
+
+/*
+ * SYSTEM CONFIGURATION VALUES - for initial load of EEPROM values
+ */
+
+/* general machine settings */
+
+//#define MAX_LINEAR_JERK 5000000	//   5,000,000 mm/(min^3)
+//#define MAX_LINEAR_JERK 1000000	//  10,000,000 mm/(min^3)
+#define MAX_LINEAR_JERK 50000000	//  50,000,000 mm/(min^3)
+//#define MAX_LINEAR_JERK 100000000	// 100,000,000 mm/(min^3)
+
+#define MAX_ANGULAR_JERK 100000000	// 100,000,000 mm/(min^3)
+//#define MIN_SEGMENT_TIME 2000		// microseconds
+#define MIN_SEGMENT_TIME 10000		// microseconds - seems to work best
+#define MM_PER_ARC_SEGMENT 0.05
+
+/* Gcode power-on defaults */
+
+#define GCODE_PLANE	CANON_PLANE_XY
+#define GCODE_UNITS 1				// mm
+#define GCODE_PATH_CONTROL PATH_CONTROL_MODE_CONTINUOUS
+//#define GCODE_PATH_CONTROL PATH_CONTROL_MODE_EXACT_STOP
+#define GCODE_TOOL 1
+#define GCODE_FEED_RATE 400			// mm/min
+#define GCODE_SPINDLE_SPEED 1500	// rpm
+
+/* axis mapping and modes */
+
+#define X_MAP_AXIS 0				// standard axis mappings (off by one)
+#define Y_MAP_AXIS 1
+#define Z_MAP_AXIS 2
+#define A_MAP_AXIS 3
+
+#define X_AXIS_MODE 0				// 0=normal mode
+#define Y_AXIS_MODE 0
+#define Z_AXIS_MODE 0				// 1= Z disable (for dry runs)
+#define A_AXIS_MODE 0				// 1= A extruder mode (2= coordinated spindle)
+
+/* motor settings */
+
+#define STEP_ANGLE 1.8				// or 0.9 in some cases
+//#define STEP_ANGLE 0.9				// or 0.9 in some cases
+#define MICROSTEP_MODE 8			// Choose one of: 8, 4, 2, 1
+
+/* robot dimensions */
+
+#define X_TRAVEL 400				// total travel in X in mm
+#define Y_TRAVEL 400
+#define Z_TRAVEL 50
+#define A_RADIUS RADIAN				// makes mm/min = degrees/min on conversion
+
+/* homing cycle settings */
+
+#define HOMING_MODE TRUE			// set TRUE for power-on-homing
+//#define HOMING_MODE FALSE			// set TRUE for power-on-homing
+#define HOMING_RATE 500				// mm/min
+#define HOMING_BACKOFF 10			// mm
+
+#define X_HOMING_ENABLE 1			// 1=enabled for that axis
+#define Y_HOMING_ENABLE 1
+#define Z_HOMING_ENABLE 0
+#define A_HOMING_ENABLE 0
+
+/* machine profiles - chose one: */
+
+//#define __LEADSCREW_20			// 1/4" 20 leadscrew table
+#define __LEADSCREW_10				// 10 TPI leadscrew table
+//#define __MAKERBOT				// Makerbot settings
+
+// Profile for 1/4-20 hardware leadscrew machines
+#ifdef __LEADSCREW_20
+#define SEEK_STEPS 1000
+#define FEED_STEPS 1000
+#define TRAVEL_PER_REV 1.27
+#endif
+
+// Profile for 10 TPI  leadscrew machines
+#ifdef __LEADSCREW_10
+#define SEEK_STEPS 1000
+#define FEED_STEPS 1000
+#define TRAVEL_PER_REV 2.54
+#endif
+
+// Makerbot profile					// Note: POLARITY is [0,0,1,1]
+#ifdef __MAKERBOT
+#define SEEK_STEPS 500
+#define FEED_STEPS 500
+#define TRAVEL_PER_REV 33			// must set Z to 1.27 (or something)
+#define HOMING_OFFSET -50
+#endif
+
+/*
+ * DERIVED VALUES (from above) - shouldn't need changing but can be overtyped
+ */
+
+#define X_SEEK_WHOLE_STEPS_PER_SEC SEEK_STEPS // max whole steps per second for G0 motion
+#define Y_SEEK_WHOLE_STEPS_PER_SEC SEEK_STEPS // (motor parameter)
+#define Z_SEEK_WHOLE_STEPS_PER_SEC SEEK_STEPS
+#define A_SEEK_WHOLE_STEPS_PER_SEC SEEK_STEPS
+
+#define X_FEED_WHOLE_STEPS_PER_SEC FEED_STEPS // max whole steps per sec for feed motion
+#define Y_FEED_WHOLE_STEPS_PER_SEC FEED_STEPS // (motor parameter)
+#define Z_FEED_WHOLE_STEPS_PER_SEC FEED_STEPS
+#define A_FEED_WHOLE_STEPS_PER_SEC FEED_STEPS
+
+#define X_STEP_ANGLE STEP_ANGLE			// degrees per whole step
+#define Y_STEP_ANGLE STEP_ANGLE 		// (motor parameter)
+#define Z_STEP_ANGLE STEP_ANGLE
+#define A_STEP_ANGLE STEP_ANGLE
+
+#define X_MICROSTEP_MODE MICROSTEP_MODE	// microstep mode 
+#define Y_MICROSTEP_MODE MICROSTEP_MODE	// (stepper driver configuration parameter)
+#define Z_MICROSTEP_MODE MICROSTEP_MODE
+#define A_MICROSTEP_MODE MICROSTEP_MODE
+
+#define X_POLARITY 0					// motor direction polarity
+#define Y_POLARITY 0
+#define Z_POLARITY 0
+#define A_POLARITY 1
+
+#define X_POWER_MODE TRUE				// 1=low power idle enabled 
+#define Y_POWER_MODE TRUE				// (robot parameter)
+#define Z_POWER_MODE TRUE
+#define A_POWER_MODE TRUE
+
+#define X_LIMIT_MODE TRUE				// 1=limit switches present and enabled
+#define Y_LIMIT_MODE TRUE				// (robot parameter)
+#define Z_LIMIT_MODE TRUE
+#define A_LIMIT_MODE FALSE
+
+#define X_TRAVEL_PER_REV TRAVEL_PER_REV	// typ. set by leadscrews or cogwheels
+#define Y_TRAVEL_PER_REV TRAVEL_PER_REV	// (robot parameter)
+#define Z_TRAVEL_PER_REV TRAVEL_PER_REV
+#define A_TRAVEL_PER_REV TRAVEL_PER_REV	// degrees moved per motor revolution
+
+//#define X_TRAVEL_PER_REV 22.86		// typ. set by leadscrews or cogwheels
+//#define Y_TRAVEL_PER_REV 48.768		// (robot parameter)
+//#define Z_TRAVEL_PER_REV 2.54
+//#define A_TRAVEL_PER_REV 2.54			// degrees moved per motor revolution
+
+#define X_TRAVEL_MAX X_TRAVEL			// full excursion from min to max 
+#define Y_TRAVEL_MAX Y_TRAVEL			// (robot parameter)
+#define Z_TRAVEL_MAX Z_TRAVEL
+#define A_TRAVEL_MAX -1					// -1 is no limit (typ for rotary axis)
+
+#define X_TRAVEL_WARN X_TRAVEL			// full excursion from min to max 
+#define Y_TRAVEL_WARN Y_TRAVEL
+#define Z_TRAVEL_WARN Z_TRAVEL
+#define A_TRAVEL_WARN -1				// -1 is no limit (typ for rotary axis)
+
+#define X_HOMING_OFFSET -(X_TRAVEL/2) 	// offset to zero from axis minimum
+#define Y_HOMING_OFFSET -(Y_TRAVEL/2)
+#define Z_HOMING_OFFSET -(Z_TRAVEL/2)
+#define A_HOMING_OFFSET 0
+
+#define X_HOMING_RATE HOMING_RATE
+#define Y_HOMING_RATE HOMING_RATE
+#define Z_HOMING_RATE HOMING_RATE
+#define A_HOMING_RATE HOMING_RATE
+
+#define X_HOMING_BACKOFF HOMING_BACKOFF
+#define Y_HOMING_BACKOFF HOMING_BACKOFF
+#define Z_HOMING_BACKOFF HOMING_BACKOFF
+#define A_HOMING_BACKOFF HOMING_BACKOFF
+
+#endif
