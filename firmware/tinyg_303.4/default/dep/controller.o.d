9ef50711c899cbbc8f33107a0542a172040ecf8b
Alden Hart <alden@macintosh-3.home>
2011-02-27 14:49:15 -0500

    working backplanner

---
+++ firmware/tinyg_303.4/default/dep/controller.o.d
@@ -0,0 +1,83 @@
+controller.o: ../controller.c \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/ctype.h ../xio.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iox256a3.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h ../xio_file.h \
+  ../xio_usart.h ../tinyg.h ../controller.h ../signals.h ../gcode.h \
+  ../config.h ../canonical_machine.h ../planner.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/string.h ../direct_drive.h \
+  ../stepper.h ../limit_switches.h ../gcode_contraptor_circle.h \
+  ../gcode_braid2d.h
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/ctype.h:
+
+../xio.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iox256a3.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+../xio_file.h:
+
+../xio_usart.h:
+
+../tinyg.h:
+
+../controller.h:
+
+../signals.h:
+
+../gcode.h:
+
+../config.h:
+
+../canonical_machine.h:
+
+../planner.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/string.h:
+
+../direct_drive.h:
+
+../stepper.h:
+
+../limit_switches.h:
+
+../gcode_contraptor_circle.h:
+
+../gcode_braid2d.h:
