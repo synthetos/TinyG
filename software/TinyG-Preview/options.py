4b93a7e0563e2f18af7634e142c74ede89b92f70
ril3y <ril3y@mbp.(none)>
2011-06-14 14:06:21 -0400

    Moved old control software to OLD, Coded xonxoff to work on TinyConsole.py

--- software/TinyG-Preview/options.py
+++
@@ -1,7 +0,0 @@
-#TinyG Options
-
-
-class TG_Options(object):
-    options = { "serialport" :"/dev/cu.usbserial-A700fhW5",
-                "speed" : 115200 }
-    
--- software/TinyGUI/V2/coms.py
+++
@@ -1,6 +0,0 @@
-#!/usr/env/python
-
-class Comms(object):
-    """All things dealing with pc to TinyG communications"""
-    def __init__(self):
-        pass
