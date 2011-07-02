f1862f3c5fe4d3f3c114843fba870667d4d47bed
aldenhart <alden08@harts.org>
2011-07-02 06:41:28 -0400

    bug fix update

---
+++ firmware/tinyg_312.04/default/dep/system.o.d
@@ -0,0 +1,23 @@
+system.o: ../system.c ../tinyg.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  ../system.h ../xmega_init.h
+
+../tinyg.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+../system.h:
+
+../xmega_init.h:
