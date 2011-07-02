1917116d55416479d313501ac7c31b35abfe2ba6
aldenhart <alden08@harts.org>
2011-07-02 06:41:28 -0400

    bug fix update

---
+++ firmware/tinyg_312.04/default/dep/config.o.d
@@ -0,0 +1,87 @@
+config.o: ../config.c \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/ctype.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/math.h ../tinyg.h \
+  ../system.h ../settings.h ../stepper.h ../xio.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iox256a3.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h ../xio_file.h \
+  ../xio_usart.h ../xio_signals.h ../gcode.h ../config.h \
+  ../canonical_machine.h ../planner.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/string.h ../controller.h \
+  ../xmega_eeprom.h
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdio.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/ctype.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdlib.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/math.h:
+
+../tinyg.h:
+
+../system.h:
+
+../settings.h:
+
+../stepper.h:
+
+../xio.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iox256a3.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+../xio_file.h:
+
+../xio_usart.h:
+
+../xio_signals.h:
+
+../gcode.h:
+
+../config.h:
+
+../canonical_machine.h:
+
+../planner.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/string.h:
+
+../controller.h:
+
+../xmega_eeprom.h:
