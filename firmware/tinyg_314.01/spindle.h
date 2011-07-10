b59dc9af2553100d4ef5af9046c38ec77019d17d
Alden Hart <alden@macintosh-3.home>
2011-07-10 14:39:33 -0400

    slave mode working - look for bugs

---
+++ firmware/tinyg_314.01/spindle.h
@@ -0,0 +1,36 @@
+/*
+ * spindle.h - spindle driver
+ * Part of TinyG project
+ *
+ * Copyright (c) 2011 Alden S. Hart Jr.
+ * Portions copyright (c) 2009 Simen Svale Skogsrud
+ *
+ * TinyG is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * TinyG is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with TinyG  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef spindle_h
+#define spindle_h 
+
+#include <avr/io.h>
+
+/*
+ * Global Scope Functions
+ */
+
+/* Note: See hardware.h for spindle port assignments and bit positions */
+
+void sp_init();
+void sp_spindle_run(uint8_t mode, double speed);
+
+#endif
