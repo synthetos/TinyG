43c81088d8bb255ac86284738baea7c47263e44a
aldenhart <a4g)hubhub>
2011-10-28 07:00:25 -0400

    fixed accel race condition

---
+++ firmware/tinyg_324.04/default/canonical_machine.d
@@ -0,0 +1,20 @@
+canonical_machine.d canonical_machine.o: .././canonical_machine.c \
+ .././tinyg.h .././gcode.h .././config.h .././planner.h \
+ .././canonical_machine.h .././controller.h .././limit_switches.h \
+ .././spindle.h
+
+.././tinyg.h:
+
+.././gcode.h:
+
+.././config.h:
+
+.././planner.h:
+
+.././canonical_machine.h:
+
+.././controller.h:
+
+.././limit_switches.h:
+
+.././spindle.h:
