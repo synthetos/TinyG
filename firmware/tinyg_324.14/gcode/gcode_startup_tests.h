b7994a9c79069993261e26d9bbacc360540cdcce
aldenhart <a4g)hubhub>
2011-11-06 06:20:04 -0500

    Argyle Socks version - fixed arc bug

---
+++ firmware/tinyg_324.14/gcode/gcode_startup_tests.h
@@ -0,0 +1,18 @@
+/*
+ * gcode_startup_tests.h - data file containing assorted tests
+ */
+
+const char PROGMEM startup_tests[] = "\
+G00 G17 G21 G40 G49 G80 G90 (initialize model)\n\
+G92 X0 Y0 Z0 (zero system)\n\
+G00 x20\n\
+y20\n\
+z20\n\
+a20\n\
+G00 x0 y0 z0 a0\n\
+G01 f100 x7\n\
+y7\n\
+z7\n\
+a7\n\
+G00 x0 y0 z0 a0\n\
+G92 x0 y0";
