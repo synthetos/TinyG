8436f3678970959a5b1b084ea60a8eb8fe0de842
aldenhart <a4g)hubhub>
2011-11-20 13:32:29 -0500

    fixed distance error bug

---
+++ firmware/tinyg_326.06/default/xmega_rtc.d
@@ -0,0 +1,8 @@
+xmega_rtc.d xmega_rtc.o: .././xmega_rtc.c .././tinyg.h \
+ .././limit_switches.h .././xmega_rtc.h
+
+.././tinyg.h:
+
+.././limit_switches.h:
+
+.././xmega_rtc.h:
---
+++ firmware/tinyg_326.06/kinematics.h
@@ -0,0 +1,41 @@
+/*
+ * kinematics.h - inverse kinematics routines
+ * Part of TinyG project
+ *
+ * Copyright (c) 2011 Alden S. Hart Jr.
+ *
+ * TinyG is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * TinyG is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with TinyG  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * At some point this whole thing ought to be renamed as the line buffer,
+ * segment buffer, motor buffer, motor command buffer, joint buffer, or 
+ * something that's more descriptive and not in conflict with the 
+ * upper-level move buffer used by the planner.
+ */
+
+#ifndef kinematics_h
+#define kinematics_h 
+
+/*
+ * Global Scope Functions
+ */
+
+uint8_t ik_kinematics(double travel[], double steps[], double microseconds);
+
+#ifdef __UNIT_TESTS
+void ik_unit_tests(void);
+#endif
+
+#endif
+
