1034994de79e177caf71d773fb9b7e5d9965a913
aldenhart <a4g)hubhub>
2011-12-23 14:05:16 -0500

    Feedhold test version 327.22  (Daisy Dukes)

---
+++ firmware/tinyg_327.22/default/xio_usb.d
@@ -0,0 +1,12 @@
+xio_usb.d xio_usb.o: .././xio_usb.c .././xio.h .././xio_file.h \
+ .././xio_usart.h .././xio_signals.h .././xmega_interrupts.h
+
+.././xio.h:
+
+.././xio_file.h:
+
+.././xio_usart.h:
+
+.././xio_signals.h:
+
+.././xmega_interrupts.h:
