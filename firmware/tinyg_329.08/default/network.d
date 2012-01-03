72511830dad6245cd9c6599fab2d51a349b52dda
aldenhart <a4g)hubhub>
2012-01-03 14:31:37 -0500

    Working on homing - not done yet

---
+++ firmware/tinyg_329.08/default/network.d
@@ -0,0 +1,14 @@
+network.d network.o: .././network.c .././xio.h .././xio_file.h \
+ .././xio_usart.h .././xio_signals.h .././encoder.h .././controller.h
+
+.././xio.h:
+
+.././xio_file.h:
+
+.././xio_usart.h:
+
+.././xio_signals.h:
+
+.././encoder.h:
+
+.././controller.h:
