63ca599a7957b71050c70923cb0437228f555dfe
aldenhart <a4g)hubhub>
2012-02-20 20:26:03 -0500

    try G92 - but still some work to go on status reports

---
+++ firmware/tinyg_331.13/default/xio_rs485.d
@@ -0,0 +1,12 @@
+xio_rs485.d xio_rs485.o: .././xio_rs485.c .././xio.h .././xio_file.h \
+ .././xio_usart.h .././xio_signals.h .././xmega_interrupts.h
+
+.././xio.h:
+
+.././xio_file.h:
+
+.././xio_usart.h:
+
+.././xio_signals.h:
+
+.././xmega_interrupts.h:
