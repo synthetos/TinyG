498ea7fee9adf6296f93727047681f33d432313b
aldenhart <a4g)hubhub>
2012-02-26 11:38:10 -0500

    added spindle and coolant commands

---
+++ firmware/tinyg_331.24/default/xmega_eeprom.d
@@ -0,0 +1,6 @@
+xmega_eeprom.d xmega_eeprom.o: .././xmega_eeprom.c .././xmega_eeprom.h \
+ .././tinyg.h
+
+.././xmega_eeprom.h:
+
+.././tinyg.h:
---
+++ firmware/tinyg_331.24/report.h
@@ -0,0 +1,45 @@
+/*
+ * report.h - rs274/ngc status report and other reporting functions
+ * Part of TinyG project
+ *
+ * Copyright (c) 2010 - 2012 Alden S. Hart Jr.
+ *
+ * TinyG is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * TinyG is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with TinyG  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef report_h
+#define report_h
+
+void rpt_init_status_report(uint8_t persist_flag);
+void rpt_decr_status_report(void);
+void rpt_force_status_report(void);
+uint8_t rpt_status_report_callback(void);
+void rpt_run_status_report(void);
+void rpt_run_multiline_status_report(void);
+void rpt_run_json_status_report(void);
+
+//void rpt_print_configs(char *str);
+//void rpt_print_machine_state(void);
+
+/* unit test setup */
+//#define __UNIT_TEST_REPORT	// uncomment to enable report unit tests
+#ifdef __UNIT_TEST_REPORT
+void sr_unit_tests(void);
+#define	REPORT_UNITS sr_unit_tests();
+#else
+#define	REPORT_UNITS
+#endif // __UNIT_TEST_REPORT
+
+#endif
+
