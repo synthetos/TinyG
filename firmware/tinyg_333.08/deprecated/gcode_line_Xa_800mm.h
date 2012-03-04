e938f87b722a30108e9acdf573a66e0a85b26626
aldenhart <a4g)hubhub>
2012-03-04 18:14:59 -0500

    release candidate for 0.93

---
+++ firmware/tinyg_333.08/deprecated/gcode_line_Xa_800mm.h
@@ -0,0 +1,111 @@
+/* 
+ * gcode_line_X_800mm.h - data file containing gcode
+ */
+
+const char PROGMEM line_X_800[] = "\
+N1 T1M6\n\
+N2 G17\n\
+N3 G21 (mm)\n\
+N4 G92X0 Y0 Z0\n\
+N5 F800.0\n\
+N6 G1 x0\n\
+N7 G1 x0.1\n\
+N8 G1 x0.2\n\
+N9 G1 x0.3\n\
+N10 G1 x0.4\n\
+N11 G1 x0.5\n\
+N12 G1 x0.6\n\
+N13 G1 x0.7\n\
+N14 G1 x0.8\n\
+N15 G1 x0.9\n\
+N16 G1 x1.0\n\
+N17 G1 x1.1\n\
+N18 G1 x1.2\n\
+N19 G1 x1.3\n\
+N20 G1 x1.4\n\
+N21 G1 x1.5\n\
+N22 G1 x1.6\n\
+N23 G1 x1.7\n\
+/N24 G1 x1.8\n\
+/N25 G1 x1.9\n\
+/N26 G1 x2.0\n\
+/N27 G1 x2.1\n\
+/N28 G1 x2.2\n\
+/N29 G1 x2.3\n\
+/N30 G1 x2.4\n\
+/N31 G1 x2.5\n\
+/N32 G1 x2.6\n\
+/N33 G1 x2.7\n\
+/N34 G1 x2.8\n\
+/N35 G1 x2.9\n\
+/N36 G1 x3.0\n\
+/N37 G1 x3.1\n\
+/N38 G1 x3.2\n\
+/N39 G1 x3.3\n\
+/N40 G1 x3.4\n\
+/N41 G1 x3.5\n\
+/N42 G1 x3.6\n\
+/N43 G1 x3.7\n\
+/N44 G1 x3.8\n\
+/N45 G1 x3.9\n\
+/N46 G1 x4.0\n\
+/N47 G1 x4.1\n\
+/N48 G1 x4.2\n\
+/N49 G1 x4.3\n\
+/N50 G1 x4.4\n\
+/N51 G1 x4.5\n\
+/N52 G1 x4.6\n\
+/N53 G1 x4.7\n\
+/N54 G1 x4.8\n\
+/N55 G1 x4.9\n\
+/N56 G1 x5.0\n\
+/N57 G1 x5.1\n\
+/N58 G1 x5.2\n\
+/N59 G1 x5.3\n\
+/N60 G1 x5.4\n\
+/N61 G1 x5.5\n\
+/N62 G1 x5.6\n\
+/N63 G1 x5.7\n\
+/N64 G1 x5.8\n\
+/N65 G1 x5.9\n\
+/N66 G1 x6.0\n\
+/N67 G1 x6.1\n\
+/N68 G1 x6.2\n\
+/N69 G1 x6.3\n\
+/N70 G1 x6.4\n\
+/N71 G1 x6.5\n\
+/N72 G1 x6.6\n\
+/N73 G1 x6.7\n\
+/N74 G1 x6.8\n\
+/N75 G1 x6.9\n\
+/N76 G1 x7.0\n\
+/N77 G1 x7.1\n\
+/N78 G1 x7.2\n\
+/N79 G1 x7.3\n\
+/N80 G1 x7.4\n\
+/N81 G1 x7.5\n\
+/N82 G1 x7.6\n\
+/N83 G1 x7.7\n\
+/N84 G1 x7.8\n\
+/N85 G1 x7.9\n\
+/N86 G1 x8.0\n\
+/N87 G1 x8.1\n\
+/N88 G1 x8.2\n\
+/N89 G1 x8.3\n\
+/N90 G1 x8.4\n\
+/N91 G1 x8.5\n\
+/N92 G1 x8.6\n\
+/N93 G1 x8.7\n\
+/N94 G1 x8.8\n\
+/N95 G1 x8.9\n\
+/N96 G1 x9.0\n\
+/N97 G1 x9.1\n\
+/N98 G1 x9.2\n\
+/N99 G1 x9.3\n\
+/N100 G1 x9.4\n\
+/N101 G1 x9.5\n\
+/N102 G1 x9.6\n\
+/N103 G1 x9.7\n\
+/N104 G1 x9.8\n\
+/N105 G1 x9.9\n\
+/N106 G1 x0 y0";
---
+++ firmware/tinyg_333.08/spindle.c
@@ -0,0 +1,91 @@
+/*
+ * canonical_spindle.c - canonical machine spindle driver
+ * Part of TinyG project
+ *
+ * Copyright (c) 2010 - 2012 Alden S. Hart Jr.
+ *
+ * TinyG is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * TinyG is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for details. You should have received a copy of the GNU General Public 
+ * License along with TinyG  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* This module is pretty much a direct lift from the grbl project.
+ * It has not been exercised or tested.
+ */
+
+#include <avr/io.h>
+#include "tinyg.h"
+#include "gpio.h"
+#include "gcode_parser.h"
+#include "canonical_machine.h"
+#include "spindle.h"
+#include "planner.h"
+#include "system.h"
+
+/* 
+ * sp_init()
+ */
+
+void cm_spindle_init()
+{
+	return;
+}
+
+/*
+ * cm_spindle_control() -  queue the spindle command to the planner buffer
+ */
+uint8_t cm_spindle_control(uint8_t spindle_mode)
+{
+	if (spindle_mode == SPINDLE_CW) {
+		mp_queue_mcode(MCODE_SPINDLE_CW);
+	} else if (spindle_mode == SPINDLE_CCW) {
+		mp_queue_mcode(MCODE_SPINDLE_CCW);
+	} else {
+		mp_queue_mcode(MCODE_SPINDLE_OFF);	// failsafe operation
+	}
+	return(TG_OK);
+}
+
+/*
+ * cm_exec_spindle_control() - execute the spindle command (called from planner)
+ */
+void cm_exec_spindle_control(uint8_t spindle_mode)
+{
+	cm_set_spindle_mode(spindle_mode);
+ 	if (spindle_mode == SPINDLE_CW) {
+		gpio_set_bit_on(SPINDLE_BIT);
+		gpio_set_bit_off(SPINDLE_DIR);
+	} else if (spindle_mode == SPINDLE_CCW) {
+		gpio_set_bit_on(SPINDLE_BIT);
+		gpio_set_bit_on(SPINDLE_DIR);
+	} else {
+		gpio_set_bit_off(SPINDLE_BIT);	// failsafe: any error causes stop
+	}
+}
+
+/*
+ * cm_set_spindle_speed() - queue the S parameter to the planner buffer
+ */
+
+uint8_t cm_set_spindle_speed(double speed)
+{
+//	if (speed > gm.max_spindle speed) {
+//		return (TG_MAX_SPINDLE_SPEED_EXCEEDED);
+//	}
+	cm_set_spindle_speed_parameter(speed);
+	return (TG_OK);
+}
+
+/*
+ * cm_exec_spindle_speed() - execute the S command (called from the planner buffer)
+ */
+void cm_exec_spindle_speed(double speed)
+{
+
+}
