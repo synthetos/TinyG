4fdce1d7c819b7a474f04e476c849f78591e2758
alden.hart <alden08@harts.org>
2012-11-15 05:37:34 -0500

    345.07 Back-ported edge branches 344.11 and 344.12 changes to homing and profiles

--- firmware/tinyg/settings/settings_default.h
+++ firmware/tinyg/settings/settings_default.h
@@ -57,7 +57,8 @@
 #define COM_IGNORE_CRLF				IGNORE_OFF		// 0=accept either CR or LF, 1=ignore CR, 2=ignoreLF
 #define COM_ENABLE_ECHO				true
 #define COM_ENABLE_XON				true
-#define COM_COMMUNICATIONS_MODE		TG_JSON_MODE	// or TG_TEXT_MODE
+#define COM_ENABLE_QR				false
+#define COM_COMMUNICATIONS_MODE		TG_TEXT_MODE	// or TG_TEXT_MODE
 
 //#define ENABLE_ACCELERATION 1			// *** NOTE: this feature is disabled in 338.11 - always equal to 1 
 
--- firmware/tinyg/settings/settings_sacidu93.h
+++ firmware/tinyg/settings/settings_sacidu93.h
@@ -60,6 +60,7 @@
 #define COM_IGNORE_CRLF				IGNORE_OFF		// 0=accept either CR or LF, 1=ignore CR, 2=ignoreLF
 #define COM_ENABLE_ECHO				true
 #define COM_ENABLE_XON				true
+#define COM_ENABLE_QR				false
 #define COM_COMMUNICATIONS_MODE		TG_TEXT_MODE
 
 //#define ENABLE_ACCELERATION 1				// 1 to enable
