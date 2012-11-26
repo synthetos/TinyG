c27450c261385ccd1fc577bcae929349b0a1e6c8
alden.hart <alden08@harts.org>
2012-11-26 07:21:28 -0500

    346.01 Clean version of USB TX handler ready for more testing

--- firmware/tinyg/xio/xio_usb.c
+++ firmware/tinyg/xio/xio_usb.c
@@ -36,7 +36,7 @@
 #include <stdbool.h>					// true and false
 #include <avr/pgmspace.h>				// precursor for xio.h
 #include <avr/interrupt.h>
-//#include <avr/sleep.h>				// needed if blocking reads & writes are used
+#include <avr/sleep.h>					// needed for blocking TX
 
 #include "xio.h"						// includes for all devices are in here
 #include "../xmega/xmega_interrupts.h"
@@ -63,39 +63,11 @@ void xio_init_usb()	// USB inits
 
 /*
  * xio_putc_usb() 
- *
- * 	Can use the generic Usart form or use a more efficient stripped down 
- *	version below it. See xio_putc_usart() for the derivation
- */
-//int xio_putc_usb(const char c, FILE *stream) 
-//{ 
-//	return xio_putc_usart(XIO_DEV_USB, c, stream);
-//}
-
-int xio_putc_usb(const char c, FILE *stream)
-{
-	USBu.usart->CTRLA = CTRLA_RXON_TXOFF;			// disable TX interrupt (mutex region)
-
-	if (--USBu.tx_buf_head == 0) { 					// advance the head & wrap
-		USBu.tx_buf_head = TX_BUFFER_SIZE-1;		// -1 avoids the off-by-one
-	}
-	if (USBu.tx_buf_head == USBu.tx_buf_tail) {		// return error if TX buffer full
-		if (++USBu.tx_buf_head == TX_BUFFER_SIZE-1) {// put the head back the way you found it
-			USBu.tx_buf_head = 0;
-		}
-		USB.signal = XIO_SIG_EAGAIN;
-		USBu.usart->CTRLA = CTRLA_RXON_TXON;
-		gpio_set_bit_on(0x01);						// turn on LED to indicate error
-		return(_FDEV_ERR);
-	};
-	USBu.tx_buf[USBu.tx_buf_head] = c;				// write to data register
-	USBu.usart->CTRLA = CTRLA_RXON_TXON;			// force interrupt to send char (doesn't work if you just |= it)
-	return (XIO_OK);
-}
-
-/* 
  * USB_TX_ISR - USB transmitter interrupt (TX) used by xio_usb_putc()
  *
+ * 	These are co-routines that work in tandem.
+ * 	xio_putc_usb() is a more efficient form derived from xio_putc_usart()
+ * 
  *	The TX interrupt dilemma: TX interrupts occur when the USART DATA register is 
  *	empty (and the ISR must disable interrupts when nothing's left to read, or they 
  *	keep firing). If the TX buffer is completely empty (TXCIF is set) then enabling
@@ -103,6 +75,34 @@ int xio_putc_usb(const char c, FILE *stream)
  *	never empties. So the routine that puts chars in the TX buffer must always force
  *	an interrupt.
  */
+// alternate form:
+//int xio_putc_usb(const char c, FILE *stream) { return xio_putc_usart(XIO_DEV_USB, c, stream);}
+
+int xio_putc_usb(const char c, FILE *stream)
+{
+	BUFFER_T next_tx_buf_head;
+
+	if ((next_tx_buf_head = (USBu.tx_buf_head)-1) == 0) { // adv. head & wrap
+		next_tx_buf_head = TX_BUFFER_SIZE-1; 	// -1 avoids the off-by-one
+	}
+	while (next_tx_buf_head == USBu.tx_buf_tail) { sleep_mode();} // sleep until there is space in the buffer
+	USBu.usart->CTRLA = CTRLA_RXON_TXOFF;			// disable TX interrupt (mutex region)
+	USBu.tx_buf_head = next_tx_buf_head;			// accept next buffer head
+	USBu.tx_buf[USBu.tx_buf_head] = c;				// write char to buffer
+	USBu.usart->CTRLA = CTRLA_RXON_TXON;			// force interrupt to send char - doesn't work if you just |= it
+	return (XIO_OK);
+
+/*
+	USBu.usart->CTRLA = CTRLA_RXON_TXOFF;			// disable TX interrupt (mutex region)
+	if (--USBu.tx_buf_head == 0) { 					// advance the head & wrap
+		USBu.tx_buf_head = TX_BUFFER_SIZE-1;		// -1 avoids the off-by-one
+	}
+	while (USBu.tx_buf_head == USBu.tx_buf_tail) { sleep_mode();} // sleep until there is space in the buffer
+	USBu.tx_buf[USBu.tx_buf_head] = c;				// write to data register
+	USBu.usart->CTRLA = CTRLA_RXON_TXON;			// force interrupt to send char (doesn't work if you just |= it)
+	return (XIO_OK);
+*/
+}
 
 ISR(USB_TX_ISR_vect) //ISR(USARTC0_DRE_vect)		// USARTC0 data register empty
 {
