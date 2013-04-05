d6fb8987284c37d82b80d1c5062e12b8ecbc08cc
alden.hart <alden08@harts.org>
2013-04-05 15:56:40 -0400

    370.17 presicion controls - second push

--- firmware/tinyg/config.c
+++ firmware/tinyg/config.c
@@ -1090,6 +1090,7 @@ static uint8_t _get_vel(cmdObj_t *cmd)
 	cmd->value = mp_get_runtime_velocity();
 	if (cm_get_units_mode() == INCHES) cmd->value *= INCH_PER_MM;
 	cmd->type = TYPE_FLOAT;
+	cmd->precision = (int8_t)pgm_read_word(&cfgArray[cmd->index].precision);
 	return (TG_OK);
 }
 
@@ -1097,6 +1098,7 @@ static uint8_t _get_pos(cmdObj_t *cmd)
 {
 	cmd->value = cm_get_runtime_work_position(_get_pos_axis(cmd->index));
 	cmd->type = TYPE_FLOAT;
+	cmd->precision = (int8_t)pgm_read_word(&cfgArray[cmd->index].precision);
 	return (TG_OK);
 }
 
@@ -1104,6 +1106,7 @@ static uint8_t _get_mpos(cmdObj_t *cmd)
 {
 	cmd->value = cm_get_runtime_machine_position(_get_pos_axis(cmd->index));
 	cmd->type = TYPE_FLOAT;
+	cmd->precision = (int8_t)pgm_read_word(&cfgArray[cmd->index].precision);
 	return (TG_OK);
 }
 
@@ -1111,6 +1114,7 @@ static uint8_t _get_ofs(cmdObj_t *cmd)
 {
 	cmd->value = cm_get_runtime_work_offset(_get_pos_axis(cmd->index));
 	cmd->type = TYPE_FLOAT;
+	cmd->precision = (int8_t)pgm_read_word(&cfgArray[cmd->index].precision);
 	return (TG_OK);
 }
 
@@ -1682,6 +1686,7 @@ static uint8_t _set_dbl(cmdObj_t *cmd)
 {
 	*((double *)pgm_read_word(&cfgArray[cmd->index].target)) = cmd->value;
 	cmd->type = TYPE_FLOAT;
+	cmd->precision = (int8_t)pgm_read_word(&cfgArray[cmd->index].precision);
 	return(TG_OK);
 }
 
@@ -1690,6 +1695,7 @@ static uint8_t _set_dbu(cmdObj_t *cmd)
 	if (cm_get_units_mode() == INCHES) { cmd->value *= MM_PER_INCH;}
 	*((double *)pgm_read_word(&cfgArray[cmd->index].target)) = cmd->value;
 	cmd->type = TYPE_FLOAT_UNITS;
+	cmd->precision = (int8_t)pgm_read_word(&cfgArray[cmd->index].precision);
 	return(TG_OK);
 }
 
@@ -2044,6 +2050,7 @@ cmdObj_t *cmd_reset_obj(cmdObj_t *cmd)	// clear a single cmdObj structure
 	cmd->type = TYPE_EMPTY;				// selective clear is much faster than calling memset
 	cmd->index = 0;
 	cmd->value = 0;
+	cmd->precision = 0;
 	cmd->token[0] = NUL;
 	cmd->group[0] = NUL;
 	cmd->stringp = NULL;
