626f444b3535c50acc66b5a1d2195e9c69596d42
alden.hart <alden08@harts.org>
2013-04-15 19:00:53 -0400

    370.18 Changed settings to default

--- firmware/tinyg/settings.h
+++ firmware/tinyg/settings.h
@@ -77,12 +77,12 @@
 
 // machine default profiles - chose only one:
 
-//#include "settings/settings_default.h"				// Default settings for release
+#include "settings/settings_default.h"				// Default settings for release
 //#include "settings/settings_lumenlabMicRoV3.h"		// Lumenlabs micRo v3
 //#include "settings/settings_othercutter.h"			// Otherlab OtherCutter
 //#include "settings/settings_otherfab.h"				// Otherlab OtherFab
 //#include "settings/settings_probotixV90.h"			// Probotix FireballV90
-#include "settings/settings_shapeoko375.h"			// Shapeoko 375mm kit
+//#include "settings/settings_shapeoko375.h"			// Shapeoko 375mm kit
 //#include "settings/settings_ultimaker.h"				// Ultimaker 3D printer
 //#include "settings/settings_zen7x12.h"				// Zen Toolworks 7x12
 
