c63acdf789e53ca794aff8ba06791ea6d1969fcc
alden.hart <alden08@harts.org>
2013-10-10 20:57:06 -0400

    interim commit

--- firmware/tinyg/text_parser.c
+++ firmware/tinyg/text_parser.c
@@ -1,8 +1,8 @@
 /*
  * text_parser.c - text parser for TinyG
- * Part of TinyG project
+ * This file is part of the TinyG project
  *
- * Copyright (c) 2010 - 2013 Alden S. Hart Jr.
+ * Copyright (c) 2010 - 2013 Alden S. Hart, Jr.
  *
  * This file ("the software") is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2 as published by the
@@ -23,9 +23,6 @@
  * SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
  * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-/*
- *	See config.h for a Config system overview and a bunch of details.
  */
 
 #include "tinyg.h"
