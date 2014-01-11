91146c59fcf00c16a277a785922ac286aa7a2048
alden.hart <alden08@harts.org>
2014-01-11 09:28:43 -0500

    409.12 testing various conditions - initial direction = false, do not adjust accumulator for direction reversals

--- firmware/tinyg/tinyg.h
+++ firmware/tinyg/tinyg.h
@@ -44,7 +44,7 @@
 
 /****** REVISIONS ******/
 
-#define TINYG_FIRMWARE_BUILD   		409.11	// testing various conditions - initial direction = true
+#define TINYG_FIRMWARE_BUILD   		409.12	// testing various conditions - initial direction = false, do not adjust accumulator for direction reversals
 #define TINYG_FIRMWARE_VERSION		0.97	// firmware major version
 #define TINYG_HARDWARE_PLATFORM		1		// hardware platform indicator (1 = Xmega series)
 #define TINYG_HARDWARE_VERSION		8		// hardware platform revision number (defaults to)
