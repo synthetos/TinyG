8a49bbd76b7de3052f9a80763593a9bc863fcdeb
alden.hart <alden08@harts.org>
2014-01-12 07:41:11 -0500

    409.23 testing various conditions - Rework from time manipulations

--- firmware/tinyg/tinyg.h
+++ firmware/tinyg/tinyg.h
@@ -44,7 +44,7 @@
 
 /****** REVISIONS ******/
 
-#define TINYG_FIRMWARE_BUILD   		409.23	// testing various conditions - Recover from timne manipulations
+#define TINYG_FIRMWARE_BUILD   		409.23	// testing various conditions - Rework from time manipulations
 #define TINYG_FIRMWARE_VERSION		0.97	// firmware major version
 #define TINYG_HARDWARE_PLATFORM		1		// hardware platform indicator (1 = Xmega series)
 #define TINYG_HARDWARE_VERSION		8		// hardware platform revision number (defaults to)
