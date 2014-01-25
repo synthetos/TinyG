f505b948853968235fe72bb5ab7cf33b8040f6fa
aldenhart <alden08@harts.org>
2014-01-25 08:03:58 -0600

    411.09 tracking g2 build 026.09

--- firmware/tinyg/text_parser.h
+++ firmware/tinyg/text_parser.h
@@ -2,7 +2,7 @@
  * text_parser.h - text parser and text mode support for tinyg2
  * This file is part of the TinyG project
  *
- * Copyright (c) 2013 Alden S. Hart, Jr.
+ * Copyright (c) 2013 - 2014 Alden S. Hart, Jr.
  *
  * This file ("the software") is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2 as published by the
