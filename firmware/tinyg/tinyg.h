0a989ee5927edff05af8256c28c7fe6b3bad505f
alden.hart <alden08@harts.org>
2014-01-29 17:33:40 -0500

    413.29 tracking g2 build 026.29

--- firmware/tinyg/tinyg.h
+++ firmware/tinyg/tinyg.h
@@ -45,7 +45,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		413.28	// tracking g2 build 026.28
+#define TINYG_FIRMWARE_BUILD   		413.29	// tracking g2 build 026.29
 #endif
 #define TINYG_FIRMWARE_VERSION		0.97	// firmware major version
 #define TINYG_HARDWARE_PLATFORM		1		// hardware platform indicator (1 = Xmega series)
