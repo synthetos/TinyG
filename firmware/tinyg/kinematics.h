8004887f8cf384fbad65be16fdc8d3ffd70f2351
alden.hart <alden08@harts.org>
2014-01-29 05:54:58 -0500

    413.23 tracking g2 build 026.23

--- firmware/tinyg/kinematics.h
+++ firmware/tinyg/kinematics.h
@@ -2,7 +2,7 @@
  * kinematics.h - inverse kinematics routines
  * This file is part of the TinyG project
  *
- * Copyright (c) 2013 Alden S. Hart, Jr.
+ * Copyright (c) 2013 - 2014 Alden S. Hart, Jr.
  *
  * This file ("the software") is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2 as published by the
