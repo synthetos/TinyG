0091201337824c126488d7b528ab954610f3fe26
alden.hart <alden08@harts.org>
2014-03-29 10:26:55 -0400

    420.11 Issue76 branch - DRO accuracy tests. Inconclusive.

--- firmware/tinyg/tinyg.h
+++ firmware/tinyg/tinyg.h
@@ -45,7 +45,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		420.10	// Issue76 branch - added skips tracking
+#define TINYG_FIRMWARE_BUILD   		420.11	// Issue76 branch - DRO accuracy tests. Inconclusive.
 #endif
 #define TINYG_FIRMWARE_VERSION		0.97	// firmware major version
 #define TINYG_HARDWARE_PLATFORM		1		// hardware platform indicator (1 = Xmega series)
