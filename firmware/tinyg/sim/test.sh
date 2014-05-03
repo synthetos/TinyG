818a77a8348a445fb7b0514b66db8d5090fd599e
Ivan Krasin <imkrasin@gmail.com>
2014-05-03 11:29:28 -0700

    simulator: all gcode files from gcode_samples directory are used as tests.
    Note: only exit code from simulator is checked. The output may be (and is likely) incorrect in many cases.

--- firmware/tinyg/sim/test.sh
+++ firmware/tinyg/sim/test.sh
@@ -6,4 +6,47 @@
 set -o nounset
 set -o errexit
 
+EXIT_CODE=0
+
+fail() {
+  EXIT_CODE=1
+  echo "FAILED"
+}
+
+echo "============================================="
+echo "Running very basic test with full output ..."
+echo "============================================="
+echo ""
+
 ./tinyg.elf < test.gcode
+
+echo -n "test.gcode -- "
+( ./tinyg.elf test.gcode && echo "OK" ) || fail
+
+echo "============================================="
+echo "OK"
+echo "============================================="
+echo ""
+
+echo ""
+echo "============================================="
+echo "Running full gcode test suite ..."
+echo "Note: currently, it only tests that the simulator exit code."
+echo "============================================="
+
+for i in ../../../gcode_samples/*.gcode
+do
+  echo -n "$i -- "
+  OUT_FILE=`basename $i`.out
+  (( ./tinyg.elf $i > $OUT_FILE 2>&1 ) && echo "OK" ) || ( fail && cat $OUT_FILE )
+done
+
+echo ""
+if [ $EXIT_CODE -eq 0 ];
+then
+  echo "All tests PASSED"
+else
+  echo "Some tests FAILED"
+fi
+
+exit $EXIT_CODE
