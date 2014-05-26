5de18e36b14b08b31a06d9d37db18c7cc781decf
alden.hart <alden08@harts.org>
2014-05-26 17:17:46 -0400

    430.10 Trapezoid generator work - combine F and B" cases

--- firmware/tinyg/plan_zoid.c
+++ firmware/tinyg/plan_zoid.c
@@ -127,56 +127,18 @@ extern "C"{
 
 void mp_calculate_trapezoid(mpBuf_t *bf)
 {
-	// F case: Block is too short (in time) to execute; T < minimum segment
-	// Force block into a single segment body with limited velocities
-	// Accept the entry velocity, limit the cruise, and go for the best exit velocity 
-	// you can get given the delta_vmax (maximum velocity slew) supportable.
-
-	float naiive_move_time = bf->length / bf->cruise_velocity;
-	if (naiive_move_time < MIN_SEGMENT_TIME_PLUS_MARGIN) {
-		bf->cruise_velocity = bf->length / MIN_SEGMENT_TIME_PLUS_MARGIN;
-		bf->exit_velocity = max(0.0, min(bf->cruise_velocity, (bf->entry_velocity - bf->delta_vmax)));
-		bf->body_length = bf->length;
-		bf->head_length = 0;
-		bf->tail_length = 0;
-		// We are violating the jerk value but since it's a single segment move we don't use it.
-		return;
-	}
-
-	// B" case: Short line, body only. See if the block fits into a single segment
-
-	if (naiive_move_time <= NOM_SEGMENT_TIME) {
-		bf->cruise_velocity = bf->length / naiive_move_time;
-		bf->exit_velocity = max(0.0, min(bf->cruise_velocity, (bf->entry_velocity - bf->delta_vmax)));
-		bf->body_length = bf->length;
-		bf->head_length = 0;
-		bf->tail_length = 0;
-		// We are violating the jerk value but since it's a single segment move we don't use it.
-		return;
-	}
-
-	// F case: Block is too short (in time) to execute; T < minimum segment
-	// B" case: Short line, body only. See if the block fits into a single segment
+	// B" case: Block is short - fits into a single body segment
+	// F case: Block is too short - run time < minimum segment time
 	
 	// Force block into a single segment body with limited velocities
 	// Accept the entry velocity, limit the cruise, and go for the best exit velocity
 	// you can get given the delta_vmax (maximum velocity slew) supportable.
-/*
+
 	float naiive_move_time = bf->length / bf->cruise_velocity;
-	if (naiive_move_time <= NOM_SEGMENT_TIME) {
-
-		if (naiive_move_time < MIN_SEGMENT_TIME_PLUS_MARGIN) {
-
-		bf->cruise_velocity = bf->length / MIN_SEGMENT_TIME_PLUS_MARGIN;
-		bf->exit_velocity = max(0.0, min(bf->cruise_velocity, (bf->entry_velocity - bf->delta_vmax)));
-		bf->body_length = bf->length;
-		bf->head_length = 0;
-		bf->tail_length = 0;
-		// We are violating the jerk value but since it's a single segment move we don't use it.
-		return;
-	}
-
-
+	if (naiive_move_time <= NOM_SEGMENT_TIME) {					// NOM_SEGMENT_TIME > B" case > MIN_SEGMENT_TIME_PLUS_MARGIN
+		if (naiive_move_time < MIN_SEGMENT_TIME_PLUS_MARGIN) {	// MIN_SEGMENT_TIME_PLUS_MARGIN > F case 
+			naiive_move_time = MIN_SEGMENT_TIME_PLUS_MARGIN;
+		}
 		bf->cruise_velocity = bf->length / naiive_move_time;
 		bf->exit_velocity = max(0.0, min(bf->cruise_velocity, (bf->entry_velocity - bf->delta_vmax)));
 		bf->body_length = bf->length;
@@ -185,7 +147,6 @@ void mp_calculate_trapezoid(mpBuf_t *bf)
 		// We are violating the jerk value but since it's a single segment move we don't use it.
 		return;
 	}
-*/
 
 	// B case:  Velocities all match (or close enough)
 	//			This occurs frequently in normal gcode files with lots of short lines
