bff0e7d473a0f8567795c93f0a5cc01c2bf7bf00
alden.hart <alden08@harts.org>
2014-06-01 16:11:04 -0400

    433.05 Build for testing

--- firmware/tinyg/tinyg.h
+++ firmware/tinyg/tinyg.h
@@ -45,7 +45,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		433.04	// Updated get_target_velocity()
+#define TINYG_FIRMWARE_BUILD   		433.05	// Build for testing
 #endif
 #define TINYG_FIRMWARE_VERSION		0.97	// firmware major version
 #define TINYG_HARDWARE_PLATFORM		1		// hardware platform indicator (1 = Xmega series)
@@ -77,7 +77,7 @@
 	#define __SILENCE_JSON_RESPONSES
 #endif
 
-#define __CANNED_STARTUP				// run any canned startup moves
+//#define __CANNED_STARTUP				// run any canned startup moves
 #define __TEST_99 							// comment out to remove diagnostic test 99
 #define __DIAGNOSTIC_PARAMETERS				// include diagnostics in config_app table
 //#define __UNIT_TESTS						// master enable for unit tests; USAGE: uncomment test in .h file
