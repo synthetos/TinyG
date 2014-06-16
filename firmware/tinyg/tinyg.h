e2f5fe855cae8d5921a40e66dc6e4cb01a1e575d
aldenhart <alden08@harts.org>
2014-06-16 10:33:22 -0400

    410.62 mp_exec_aline() end condition tests

--- firmware/tinyg/tinyg.h
+++ firmware/tinyg/tinyg.h
@@ -45,7 +45,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		410.61	// plan_line.c
+#define TINYG_FIRMWARE_BUILD   		410.62	// mp_exec_aline() end condition tests
 #endif
 #define TINYG_FIRMWARE_VERSION		0.97	// firmware major version
 #define TINYG_HARDWARE_PLATFORM		1		// hardware platform indicator (1 = Xmega series)
