524b076d6843fa93f6abe494d08e0dcecf1946c7
alden.hart <alden08@harts.org>
2015-02-18 20:31:13 -0500

    440.01 feature branch for adding new arc code - baseline

--- firmware/tinyg/tinyg.h
+++ firmware/tinyg/tinyg.h
@@ -45,7 +45,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD   		438.11	// tuned up Shapeoko2 profile settings; updated profiles for power management
+#define TINYG_FIRMWARE_BUILD   		440.01	// feature branch for adding new arc code - baseline
 
 #endif
 #define TINYG_FIRMWARE_VERSION		0.97					// firmware major version
