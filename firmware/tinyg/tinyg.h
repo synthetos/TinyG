3d7da610dd4dc36d3ef032da9a5fde3bcab51fee
alden.hart <alden08@harts.org>
2015-08-08 15:49:18 -0400

    444.18 spindle code - first commit

--- firmware/tinyg/tinyg.h
+++ firmware/tinyg/tinyg.h
@@ -87,7 +87,7 @@
 /****** REVISIONS ******/
 
 #ifndef TINYG_FIRMWARE_BUILD
-#define TINYG_FIRMWARE_BUILD        444.18	// new spindle code
+#define TINYG_FIRMWARE_BUILD        444.18	// new spindle code - first commit
 
 #endif
 #define TINYG_FIRMWARE_VERSION		0.98					// firmware major version
